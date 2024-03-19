5e14011c331c5f7f01ab5fffed9044c67824123f
Critical2020 <jmvazquez3001@gmail.com>
2024-03-18 22:57:21 -0400

    DB Min Implementation

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DBWeather</title>
+    <style>
+        table, th, td {
+            border: 1px solid;
+        }
+    </style>
+</head>
+<body>
+    <h2>MarWeather</h2>
+
+    <form action="" method="post">
+        <label for="field">Search:</label>
+        <input type="text" name="field" id="field">
+    </form>
+
+    <p></p>
+
+    <table>
+        <thead>
+            <th>Country</th>
+            <th>Time</th>
+            <th>Weather</th>
+            <th>Humidity</th>
+            <th>Wind Speed</th>
+        </thead>
+    </table>
+
+</body>
+</html>
